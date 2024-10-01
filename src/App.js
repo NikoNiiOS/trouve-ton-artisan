d6c5352c65ce35ce8203eaa42b602f49b1b481fe
NikoNiiOS <ryxxal@gmail.com>
2024-10-01 11:53:53 +0200

    Home page
    formating json file,
    creating home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './pages/Home';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import data from "./data/datas.json";
+import "./App.css";
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
     <div className="App">
-      <Header/>
+      <Header onSearch={handleSearch}/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import data from "../data/datas.json";
+import Card from '../components/CardArtisan'
 import "../styles/home.css";
 
-function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
+function Home() {
+  const topArtisans = data.filter((artisan) => artisan.top);
+
+  const artisans = topArtisans.map((artisan) => {
+    return (
+      <Card
+        name={artisan.name}
+        specialty={artisan.specialty}
+        location={artisan.location}
+        btn="En savoir plus"
+        key={artisan.id}
+        id={artisan.id}
+        note={artisan.note}
+      />
     );
+  });
+
+  return (
+    <section className="page">
+      <h1>Les artisans de la région</h1>
+      <div className="sep1"></div>
+      <div>
+        <h2>Comment trouver mon artisan ?</h2>
+        <ol>
+          <li>Choisir la catégorie d’artisanat dans le menu.</li>
+          <li>Choisir un artisan.</li>
+          <li>Le contacter via le formulaire de contact.</li>
+          <li>Une réponse vous sera apportée sous 48h.</li>
+        </ol>
+      </div>
+      <div className="sep2"></div>
+      <h2>Les artisans du mois</h2>
+      <ul className="row list-card">{artisans}</ul>
+    </section>
+  );
 }
 
 export default Home;
